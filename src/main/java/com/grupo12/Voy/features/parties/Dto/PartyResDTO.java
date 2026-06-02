40e69c50f201c6f35fe710bd23b9269b31c93680
Marianomdp <mariano.collovatti@gmail.com>
2026-06-01 21:54:17 -0300

    PartyUserDto created and implement

--- src/main/java/com/grupo12/Voy/features/parties/Dto/PartyResDTO.java
+++ src/main/java/com/grupo12/Voy/features/parties/Dto/PartyResDTO.java
@@ -1,26 +1,19 @@
 package com.grupo12.Voy.features.parties.Dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.UUID;
-@NoArgsConstructor
-@Data
-@AllArgsConstructor
 
-public class PartyResDTO {
-    private UUID idExternal;
-    private String title;
-    private String description;
-    private String city;
-    private String adress;
-    private LocalDateTime dateTime;
-    private Integer guestLimit;
-    private Boolean partyAccesibility;
-    private Boolean state;
-    private String organizerName; ///deberia mostrar username no el email, pero por ahora dejo el email como identificador
-    private Set<String> tags;
-}
+public record PartyResDTO (
+     UUID idExternal,
+     String title,
+     String description,
+     String city,
+     String adress,
+     LocalDateTime dateTime,
+     Integer guestLimit,
+     Boolean partyAccesibility,
+     Boolean state,
+     String organizerName, ///deberia mostrar username no el email, pero por ahora dejo el email como identificador
+     Set<String> tags)
+    {}
