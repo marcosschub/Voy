88b68ebb603a393abf10430876dbd393d83f07a2
marcos <schubmarcos@gmail.com>
2026-06-05 16:13:11 -0300

    feat/ExceptionHandlerGlobal y ErrorDetails creados

--- src/main/java/com/grupo12/Voy/common/models/ErrorDetails.java
+++ src/main/java/com/grupo12/Voy/common/models/ErrorDetails.java
@@ -1,4 +1,13 @@
 package com.grupo12.Voy.common.models;
 
-public class ErrorDetails {
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record ErrorDetails(String message,
+                           String endpoint,
+                           LocalDateTime dateTime){
+
+
 }
