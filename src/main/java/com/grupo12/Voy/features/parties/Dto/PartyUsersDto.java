5cbc33b6087660cd920d33298cbc471c95f8abb0
marcos <schubmarcos@gmail.com>
2026-06-05 21:51:39 -0300

    fixes en PartyMapper y PartyUsersDto

--- src/main/java/com/grupo12/Voy/features/parties/Dto/PartyUsersDto.java
+++ src/main/java/com/grupo12/Voy/features/parties/Dto/PartyUsersDto.java
@@ -1,13 +1,14 @@
 package com.grupo12.Voy.features.parties.Dto;
 
+import com.grupo12.Voy.features.users.Dto.UserDto;
+
 import java.time.LocalDateTime;
 
 public record PartyUsersDto(
         String title,
-        String descripcion,
+        String description,
         String city,
         String adress,
         LocalDateTime dateTime,
-        String organizer
-) {
+        UserDto organizer) {
 }
