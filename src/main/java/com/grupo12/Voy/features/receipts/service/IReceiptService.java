c15bb5654d8c81d7917e374615eb2262c985543c
marcos <schubmarcos@gmail.com>
2026-06-08 00:50:49 -0300

    Feature en specification de receipts

--- src/main/java/com/grupo12/Voy/features/receipts/service/IReceiptService.java
+++ src/main/java/com/grupo12/Voy/features/receipts/service/IReceiptService.java
@@ -3,11 +3,18 @@ package com.grupo12.Voy.features.receipts.service;
 import com.grupo12.Voy.features.receipts.DTO.ReceiptRequestDTO;
 import com.grupo12.Voy.features.receipts.DTO.ReceiptResponseDTO;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public interface IReceiptService {
-    List<ReceiptResponseDTO> getAll();
+    public List<ReceiptResponseDTO> getAll(
+            UUID externalId, String paymentMethod,
+            BigDecimal minPrice, BigDecimal maxPrice,
+            BigDecimal minFinalPrice, BigDecimal maxFinalPrice,
+            LocalDateTime from, LocalDateTime to,
+            Integer minQuantity, Integer maxQuantity);
     ReceiptResponseDTO getByExternalId(UUID id);
     List<ReceiptResponseDTO> getByPaymentMethod(String method);
     ReceiptResponseDTO createReceipt(ReceiptRequestDTO dto);
