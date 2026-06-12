c6901052b4a2d1887bdb60b968c61609992dc7b0
marcos <schubmarcos@gmail.com>
2026-06-11 21:00:08 -0300

    Fixes security funcionando

--- src/main/java/com/grupo12/Voy/features/users/Controller/UserController.java
+++ src/main/java/com/grupo12/Voy/features/users/Controller/UserController.java
@@ -1,6 +1,7 @@
 package com.grupo12.Voy.features.users.Controller;
 
 
+import com.grupo12.Voy.common.security.models.CredentialsEntity;
 import com.grupo12.Voy.features.parties.Dto.PartyUsersDto;
 import com.grupo12.Voy.features.receipts.DTO.ReceiptResponseDTO;
 import com.grupo12.Voy.features.tickets.models.DTO.TicketUsersDto;
@@ -15,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -38,8 +40,15 @@ public class UserController {
 
 
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN') or #id == authentication.principal.userId")
-    ResponseEntity<Void> deleteUser(@PathVariable UUID id){
+    @PreAuthorize("hasRole('ADMIN')")
+    ResponseEntity<Void> deleteUserByAdmin(@PathVariable UUID id){
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping
+    @PreAuthorize("hasRole('USER')")
+    ResponseEntity<Void> deleteUser(@AuthenticationPrincipal(expression = "usuario.externalId") UUID id){
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
@@ -55,6 +64,8 @@ public class UserController {
                                              @RequestBody @Valid UserUpdateDto userUpdateDto){
         return ResponseEntity.ok(userService.updateUser(idExternal, userUpdateDto));
     }
+
+
 
     @GetMapping("/{idExternal}/follows")
     @PreAuthorize("hasRole('USER')")
